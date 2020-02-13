23679997e8817dad4c17a7faaca86e1f09cdb217
Nicholas Burness <53260549+NickBurness@users.noreply.github.com>
2020-02-13 09:40:01 +0000

    add test file for challenge 2

---
+++ Euler100.Tests/EvenFibonacciNumbersTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Euler100Classes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Euler100.Tests
+{
+    [TestClass]
+    public class TotalOfEvenFibonacciNumbersBelow4Million
+    {
+        private readonly EvenFibonacciNumbers _evenFibonacciNumbers;
+
+        public TotalOfEvenFibonacciNumbersBelow4Million()
+        {
+            _evenFibonacciNumbers = new EvenFibonacciNumbers();
+        }
+
+        [TestMethod]
+        public void Challenge2_TotalOfEvenFibonacciNumbersEquals4613732()
+        {
+            var result = _evenFibonacciNumbers.Challenge2();
+
+            Assert.AreEqual(4613732, result);
+        }
+    }
+}
--- Euler100Console/Program.cs
+++ Euler100Console/Program.cs
@@ -7,10 +7,15 @@ namespace Euler100Console
     {
         static void Main(string[] args)
         {
+            // Challenge 1 
 			var test = new MultiplesOf3And5();
 			var result = test.Challenge1();
-
 			Console.WriteLine($"result is {result}");
+
+            // Challenge 2 
+            var test1 = new EvenFibonacciNumbers();
+            var result1 = test1.Challenge2();
+            Console.WriteLine($"The total value of even fibonacci numbers below 4 million is {result1}");
 		}
     }
 }
